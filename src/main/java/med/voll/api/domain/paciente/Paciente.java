a25b467ef9ff37deb464bbb430b5baf5df03b10f
Diego Menezes <42484737+diegomenezesa@users.noreply.github.com>
2023-10-09 17:49:12 -0300

    Implementar Agendamento de Consultas

---
+++ src/main/java/med/voll/api/domain/paciente/Paciente.java
@@ -0,0 +1,36 @@
+package med.voll.api.domain.paciente;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import med.voll.api.domain.endereco.Endereco;
+
+@Table(name = "pacientes")
+@Entity(name = "Paciente")
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+@EqualsAndHashCode(of = "id")
+public class Paciente {
+    @Id @GeneratedValue (strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String nome;
+    private String email;
+    private String cpf;
+    private String telefone;
+    @Embedded
+    private Endereco endereco;
+    private Boolean ativo;
+
+    public Paciente(DadosCadastroPaciente dados){
+        this.ativo = true;
+        this.nome = dados.nome();
+        this.email = dados.email();
+        this.cpf = dados.cpf();
+        this.telefone = dados.telefone();
+        this.endereco = new Endereco(dados.endereco());
+
+    }
+}
